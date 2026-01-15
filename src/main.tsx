b63ee3b63176194250cd0768b4dc13c6ff1d2364
Natalya Botsman <botsmannatashaa@gmail.com>
2026-01-16 01:58:43 +0600

    Wrapped main tsx into Provider and Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux';
+import {store} from './app/store.ts';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App/>
+    <Provider store={store}>
+      <Toaster position="top-right" richColors />
+      <App />
+    </Provider>
   </BrowserRouter>,
 )
