16dba39a39f842b97070f829c29dad063f82b9f2
Natalya Botsman <botsmannatashaa@gmail.com>
2026-01-15 14:36:22 +0600

    Removed backend dir

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-primary p-10">
-      hello
-    </div>
-  )
-}
-
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="bg-white p-10"></div>
+  )
+}
+
+export default App
