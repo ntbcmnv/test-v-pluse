fdeab0a8bb203b99631ed5264a6320fa698a27b9
Natalya Botsman <botsmannatashaa@gmail.com>
2026-01-18 23:33:58 +0600

    Added main app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import {Characters} from '@/modules';
-
+import {CharacterPage, EpisodePage, LocationPage} from '@/modules';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Header, SearchPage} from '@/shared';
 
 function App() {
-
   return (
-    <div className="p-3">
-      <Characters/>
-    </div>
+    <>
+      <Header/>
+      <div className="max-w-5xl mx-auto my-10">
+        <Routes>
+          <Route path="/" element={<Navigate to="/characters" replace />} />
+
+          <Route path="/characters" element={<SearchPage type="characters"/>}/>
+          <Route path="/episodes" element={<SearchPage type="episodes"/>}/>
+          <Route path="/locations" element={<SearchPage type="locations"/>}/>
+
+          <Route path="/characters/:id" element={<CharacterPage/>}/>
+          <Route path="/episodes/:id" element={<EpisodePage/>}/>
+          <Route path="/locations/:id" element={<LocationPage/>}/>
+        </Routes>
+      </div>
+    </>
   );
 }
 
